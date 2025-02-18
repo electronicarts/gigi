1793b2df6be1a78c2a51455180cd18f85173654b
Ivar Jönsson <ivarj@playdead.com>
2025-02-18 14:51:04 +0100

    Fixed incorrect selection index when having a search query.

--- GigiEdit/main.cpp
+++ GigiEdit/main.cpp
@@ -1713,7 +1713,7 @@ struct Example :
 
                 bool isSelected = data.index == selectedIndex;
                 if (ImGui::Selectable(data.name.c_str(), &isSelected))
-                    selectedIndex = index;
+                    selectedIndex = data.index;
 
                 if (isSelected)
                     ImGui::SetItemDefaultFocus();
